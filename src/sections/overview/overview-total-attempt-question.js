64fee3874f93c70e6f58816a748cc88a58199dea
Ranjeet Gautam <ranjeet.gautam@indiamart.com>
2024-08-08 23:14:36 +0530

    updated text attempted questions

--- src/sections/overview/overview-total-attempt-question.js
+++ src/sections/overview/overview-total-attempt-question.js
@@ -11,7 +11,7 @@ export const OverviewTotalAttemptQuestions = (props) => {
         <Stack alignItems="flex-start" direction="row" justifyContent="space-between" spacing={3}>
           <Stack spacing={1}>
             <Typography color="text.secondary" variant="overline">
-              Total Profit
+              Attempt Questions
             </Typography>
             <Typography variant="h4">{value}</Typography>
           </Stack>
